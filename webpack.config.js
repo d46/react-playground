bab5b7e9d1143a7abf51226cd0af9c657bd65f83
daghan.gunay <Dghn12-12>
2017-05-26 11:10:12 +0300

    webpack.config added && package.json inited

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'target')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};

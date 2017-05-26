aee3d491c117945d10b5a3c2a20378923921ca92
daghan.gunay <Dghn12-12>
2017-05-26 16:54:27 +0300

    components and props

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 export default class App extends React.Component {
     render() {
         return (
-            <h1>Hello World</h1>);
+            <h1>Hello World, {this.props.name}</h1>
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="Perez" />, document.getElementById('root'));

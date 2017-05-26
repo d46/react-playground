fc5fa67e2619eec294155fce17eae1e03305e98f
daghan.gunay <Dghn12-12>
2017-05-26 17:31:39 +0300

    two way data binding with event

--- src/main.js
+++ src/main.js
@@ -2,17 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0,
+            multiplyVal: 0
+        }
+        this.doubleCount = this.doubleCount.bind(this)
+        this.multiply = this.multiply.bind(this)
+        this.multiplyValChangeHandler = this.multiplyValChangeHandler.bind(this)
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timerID)
+    }
+
+    tick() {
+        this.setState((prevState, props) => ({
+            count: ++prevState.count
+        }))
+    }
+
+    doubleCount() {
+        this.setState((prevState, props) => ({
+            count: prevState.count * 2
+        }))
+    }
+
+    multiply() {
+        this.setState((prevState, props) => ({
+            count: prevState.count * prevState.multiplyVal
+        }));
+    }
+
+    multiplyValChangeHandler(event) {
+        this.setState({multiplyVal: event.target.value});
+    }
+
+    render(doubleCount) {
         return (
-            <h1>Hello World, {this.props.name}</h1>
+            <div>
+                <h1>Hello World, {this.props.name}</h1>
+                <span>Counter Tick: {this.state.count} </span>
+                <button onClick={this.doubleCount}>Double</button>
+                <br/>
+                <input type="text" value={this.state.multiplyVal} onChange={this.multiplyValChangeHandler}/>
+                <button onClick={this.multiply}>Multiply with Value</button>
+            </div>
         );
     }
+
 }
 
 ReactDOM.render(
     <div>
-        <App name="Perez" />
-        <App name="Perez" />
-        <App name="Perez" />
+        <App name="Perez"/>
     </div>
     , document.getElementById('root'));
